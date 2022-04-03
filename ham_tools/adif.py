e00486219c7e474724af63ecbdfef6196aa5845e
Nick Pegg <nick@nickpegg.com>
2022-04-02 21:13:20 -0700

    add tests, fix merging, add output via string or file

--- ham_tools/adif.py
+++ ham_tools/adif.py
@@ -5,8 +5,10 @@ Reference: https://www.adif.org/adif
 Description of the file format: http://www.adif.org/312/ADIF_312.htm#ADI_File_Format
 """
 
+import logging
 import re
 from collections import defaultdict
+from copy import copy
 from dataclasses import dataclass, field
 from datetime import date, datetime, time, timedelta
 from enum import Enum
@@ -15,6 +17,8 @@ from pathlib import Path
 from typing import Optional, TextIO, Union
 
 from ham_tools.version import VERSION
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -27,6 +31,11 @@ class AdifFile:
 
     # Any text before the first specifier in the header
     comment: str = ""
+
+    def __str__(self) -> str:
+        s = StringIO()
+        self._to_file_obj(s)
+        return s.getvalue()
 
     @classmethod
     def from_string(cls, contents: str) -> "AdifFile":
@@ -88,6 +97,40 @@ class AdifFile:
         return adif_file
 
     # TODO: output methods - to string, to file
+    def to_file(self, file_path: Path) -> None:
+        with file_path.open() as f:
+            self._to_file_obj(f)
+
+    def _to_file_obj(self, f: TextIO) -> None:
+        """
+        Write the ADIF file to the given file object
+        """
+        f.write(f"{self.comment}\n")
+        if self.version:
+            f.write(make_field("adif_ver", self.version) + "\n")
+
+        if not self.created:
+            self.created = datetime.now()
+        ts = self.created.strftime("%Y%m%d %H%M%S")
+        f.write(make_field("created_timestamp", ts) + "\n")
+
+        if self.program_id:
+            f.write(make_field("programid", self.program_id) + "\n")
+
+        if self.program_version:
+            f.write(make_field("programversion", self.program_version))
+        f.write("<eoh>\n")
+
+        for record in self.records:
+            f.write(str(record) + "\n")
+
+    def copy(self) -> "AdifFile":
+        """
+        Return a copy of this AdifFile
+        """
+        new = copy(self)
+        new.records = [r.copy() for r in self.records]
+        return new
 
     def merge(self, other: "AdifFile", time_match_min: int = 15) -> None:
         """
@@ -114,29 +157,49 @@ class AdifFile:
         # other record has no match, just append it to ours.
         for k, other_records in other_buckets.items():
             if k not in my_buckets:
+                logger.debug(f"{k} not found in my buckets")
+                print(f"{k} not found in my buckets")
                 self.records.extend(other_records)
                 continue
 
-            for my_record in my_buckets[k]:
-                for other_record in other_records:
+            for other_record in other_records:
+                # Try to find a matching record in our records
+                match = None
+                for my_record in my_buckets[k]:
+                    # Sanity check
+                    if (
+                        my_record.fields.get("callsign")
+                        != other_record.fields.get("callsign")
+                        or my_record.fields.get("mode")
+                        != other_record.fields.get("mode")
+                        or my_record.fields.get("band")
+                        != other_record.fields.get("band")
+                        or my_record.qso_date != other_record.qso_date
+                    ):
+                        raise ValueError(
+                            "records differ despite having the same match key. This is "
+                            "a bug"
+                        )
+
                     if other_record.time_on == my_record.time_on:
-                        my_record.merge(other_record)
-                        continue
+                        match = my_record
+                        break
                     elif other_record.datetime is None or my_record.datetime is None:
                         # Can't compare missing times, skip
                         continue
 
                     min_time = my_record.datetime - timedelta(minutes=time_match_min)
-                    max_time = my_record.datetime - timedelta(minutes=time_match_min)
+                    max_time = my_record.datetime + timedelta(minutes=time_match_min)
                     if (
                         other_record.datetime <= max_time
                         and other_record.datetime >= min_time
                     ):
-                        # We have a match!
-                        my_record.merge(other_record)
-                    else:
-                        # No match, append other record to our list
-                        self.records.append(other_record)
+                        match = my_record
+
+                if match:
+                    match.merge(other_record)
+                else:
+                    self.records.append(other_record)
 
         # Sort all QSOs in this file by (date, time_on) ascending
         self.records = sorted(self.records, key=lambda r: (r.qso_date, r.time_on))
@@ -158,11 +221,23 @@ class AdifRecord:
         "comment",
     }
 
+    def __str__(self) -> str:
+        buf = ""
+        for k, v in self.fields.items():
+            buf += make_field(k, v) + " "
+        buf += "<eor>"
+        return buf
+
     def __getitem__(self, key: str) -> str:
         return self.fields[key]
 
     def __setitem__(self, key: str, value: str) -> None:
         self.fields[key] = value
+
+    def copy(self) -> "AdifRecord":
+        new = copy(self)
+        new.fields = self.fields.copy()
+        return new
 
     def merge(self, other: "AdifRecord", force_overwrite: bool = False) -> None:
         """
@@ -310,3 +385,10 @@ def parse_time(time_str: str) -> time:
         return datetime.strptime(time_str, "%H%M").time()
     else:
         return datetime.strptime(time_str, "%H%M%S").time()
+
+
+def make_field(name: str, value: str) -> str:
+    """
+    Return an ADIF field/value, like "<adif_ver:5>value"
+    """
+    return f"<{name}:{len(value)}>{value}"
--- tests/test_adif.py
+++ tests/test_adif.py
@@ -109,9 +109,154 @@ def test_record_merge() -> None:
     assert r1["foo"] == "x"
 
 
-def test_file_merge() -> None:
-    # TODO
-    pass
+class TestAdifFileMerge:
+    @pytest.fixture
+    def example_record(self) -> AdifRecord:
+        return AdifRecord(
+            {
+                "callsign": "n0foo",
+                "qso_date": "20220401",
+                "time_on": "181403",
+            }
+        )
+
+    @pytest.fixture
+    def example_file(self, example_record: AdifRecord) -> AdifFile:
+        return AdifFile(
+            records=[
+                example_record,
+                AdifRecord(
+                    {
+                        "callsign": "n0foo",
+                        "mode": "FT8",
+                        "band": "10m",
+                        "qso_date": "20220401",
+                        "time_on": "131415",
+                        "time_off": "131545",
+                    }
+                ),
+                AdifRecord(
+                    {
+                        "callsign": "n0foo",
+                        "mode": "FT8",
+                        "band": "30m",
+                        "qso_date": "20220401",
+                        "time_on": "131415",
+                        "time_off": "131545",
+                    }
+                ),
+                AdifRecord(
+                    {
+                        "callsign": "n0foo",
+                        "mode": "FT8",
+                        "band": "40m",
+                        "qso_date": "20220401",
+                        "time_on": "131415",
+                        "time_off": "131545",
+                    }
+                ),
+            ]
+        )
+
+    @pytest.fixture
+    def empty_file(self) -> AdifFile:
+        return AdifFile()
+
+    def test_empty_files(
+        self, empty_file: AdifFile, example_record: AdifRecord
+    ) -> None:
+        f = AdifFile()
+        f.merge(empty_file)
+        assert f == empty_file
+
+        f = AdifFile(records=[example_record])
+        f.merge(empty_file)
+        assert f == AdifFile(records=[example_record])
+
+        f2 = empty_file.copy()
+        f2.merge(f)
+        assert f2 == f
+
+    def test_basic_merge(
+        self, example_file: AdifFile, example_record: AdifRecord
+    ) -> None:
+        """
+        Merge in a file with one matching and one new record
+        """
+        # this record is similar to the example, but later in the day
+        new_record = example_record.copy()
+        new_record["time_on"] = "235000"
+        assert example_record != new_record
+        other = AdifFile(records=[example_record, new_record])
+
+        start_count = len(example_file.records)
+        assert example_record in example_file.records
+        example_file.merge(other)
+        assert new_record in example_file.records
+        assert len(example_file.records) == start_count + 1
+
+        # Merging again doesn't add records
+        example_file.merge(other)
+        assert len(example_file.records) == start_count + 1
+
+    def test_time_offset(self, example_file: AdifFile) -> None:
+        """
+        Two similar records with close enough time should get merged together. The time
+        from the original record should persist.
+        """
+        r1 = AdifRecord(
+            {
+                "callsign": "n0foo",
+                "mode": "10m",
+                "qso_date": "20220401",
+                "time_on": "123400",
+            }
+        )
+        r2 = r1.copy()
+        r2["time_on"] = "123000"
+
+        f1 = AdifFile(records=[r1])
+        f2 = AdifFile(records=[r2])
+
+        f1.merge(f2)
+        assert len(f1.records) == 1
+        assert f1.records == [r1]
+
+        # Check for dates after too
+        f1.records = [r1]
+        r2["time_on"] = "124000"
+        f1.merge(f2)
+        assert len(f1.records) == 1
+        assert f1.records == [r1]
+
+        # Outside of range
+        f1.records = [r1]
+        r2["time_on"] = "125000"
+        f1.merge(f2)
+        assert len(f1.records) == 2
+        assert f1.records == [r1, r2]
+
+    def test_idempotency(self, example_record: AdifRecord) -> None:
+        """
+        Test merging a file twice, and make sure its records appear only once after
+        getting merged
+        """
+        # Duplicated records in other should get merged down
+        f = AdifFile(records=[example_record])
+        f2 = AdifFile(records=[example_record, example_record])
+        f.merge(f2)
+        assert len(f.records) == 1
+        assert f == AdifFile(records=[example_record])
+
+        # Merging again should be okay
+        f.merge(f2)
+        assert f == AdifFile(records=[example_record])
+
+    def test_bad_time_idempotency(self, example_file: AdifFile) -> None:
+        """
+        When a QSO with a bad time is merged in, it should be idempotent and not keep
+        getting appended
+        """
 
 
 @pytest.mark.parametrize(
@@ -151,14 +296,20 @@ def test_parse_time(given: str, expected: Union[time, Type[Exception]]) -> None:
     else:
         assert False, f"Unexpected expected: {expected}"
 
+
 @pytest.mark.parametrize(
     "given, expected",
     [
         (
             AdifRecord(
-                fields={"callsign": "n0foo", "band": "10m", "mode": "ssb", "qso_date": "20220401"}
+                fields={
+                    "callsign": "n0foo",
+                    "band": "10m",
+                    "mode": "ssb",
+                    "qso_date": "20220401",
+                }
             ),
-            "N0FOO-10m-SSB-20220401"
+            "N0FOO-10m-SSB-20220401",
         ),
         (AdifRecord(fields={"callsign": "n0foo", "mode": "ssb"}), "N0FOO--SSB-"),
         (AdifRecord(), "---"),
@@ -166,3 +317,14 @@ def test_parse_time(given: str, expected: Union[time, Type[Exception]]) -> None:
 )
 def test_record_match_key(given: AdifRecord, expected: str) -> None:
     assert given._match_key() == expected
+
+
+def test_file_str() -> None:
+    path = Path(Path(__file__).parent, "data/test.adi")
+    adif = AdifFile.from_file(path)
+
+    s = str(adif)
+    assert "<adif_ver:5>3.1.1" in s
+    assert "<created_timestamp:15>20220312 182109" in s
+    assert "<call:4>NU6V" in s
+    assert "<mode:3>FT8" in s
